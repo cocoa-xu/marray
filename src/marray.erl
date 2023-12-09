d83ec92e20e8eb2770b37bc78bce2268e6ec121a
Cocoa <i@uwucocoa.moe>
2023-12-09 23:49:38 +0800

    added some tests

--- src/marray.erl
+++ src/marray.erl
@@ -5,6 +5,7 @@
 
 -module(marray).
 -export([
+    new/1,
     new/2,
     from_list/1,
     to_list/1,
@@ -15,6 +16,12 @@
     sort/1,
     reverse/1
 ]).
+-ifdef(TEST).
+-include_lib("eunit/include/eunit.hrl").
+-endif.
+
+new(Capacity) ->
+    new(Capacity, default).
 
 new(Capacity, DefaultVal) ->
     InitVal =
@@ -49,3 +56,78 @@ sort(MArray) ->
 
 reverse(MArray) ->
     marray_nif:marray_reverse(MArray).
+
+-ifdef(EUNIT).
+new_test() ->
+    Capacity = 10,
+    M = new(Capacity),
+    ?assertEqual(marray:size(M), Capacity),
+    ?assertEqual(marray:to_list(M), lists:duplicate(Capacity, 0)),
+    M1 = new(Capacity, 1),
+    ?assertEqual(marray:size(M1), Capacity),
+    ?assertEqual(marray:to_list(M1), lists:duplicate(Capacity, 1)).
+
+from_list_test() ->
+    List = [1, 2, 3, 4, 5],
+    M = from_list(List),
+    ?assertEqual(marray:size(M), length(List)),
+    ?assertEqual(marray:to_list(M), List).
+
+to_list_test() ->
+    List = [1, 2, 3, 4, 5],
+    M = from_list(List),
+    ?assertEqual(marray:to_list(M), List).
+
+set_test() ->
+    List = [1, 2, 3, 4, 5],
+    M = from_list(List),
+    ?assertEqual(marray:get(M, 0), 1),
+    ?assertEqual(marray:get(M, 1), 2),
+    ?assertEqual(marray:get(M, 2), 3),
+    ?assertEqual(marray:get(M, 3), 4),
+    ?assertEqual(marray:get(M, 4), 5),
+    marray:set(M, 0, 10),
+    marray:set(M, 1, 20),
+    marray:set(M, 2, 30),
+    marray:set(M, 3, 40),
+    marray:set(M, 4, 50),
+    ?assertEqual(marray:get(M, 0), 10),
+    ?assertEqual(marray:get(M, 1), 20),
+    ?assertEqual(marray:get(M, 2), 30),
+    ?assertEqual(marray:get(M, 3), 40),
+    ?assertEqual(marray:get(M, 4), 50).
+
+swap_test() ->
+    List = [1, 2, 3, 4, 5],
+    M = from_list(List),
+    ?assertEqual(marray:get(M, 0), 1),
+    ?assertEqual(marray:get(M, 1), 2),
+    ?assertEqual(marray:get(M, 2), 3),
+    ?assertEqual(marray:get(M, 3), 4),
+    ?assertEqual(marray:get(M, 4), 5),
+    marray:swap(M, 0, 4),
+    marray:swap(M, 1, 3),
+    ?assertEqual(marray:get(M, 0), 5),
+    ?assertEqual(marray:get(M, 1), 4),
+    ?assertEqual(marray:get(M, 2), 3),
+    ?assertEqual(marray:get(M, 3), 2),
+    ?assertEqual(marray:get(M, 4), 1).
+
+size_test() ->
+    List = [1, 2, 3, 4, 5],
+    M = from_list(List),
+    ?assertEqual(marray:size(M), length(List)).
+
+sort_test() ->
+    List = [5, 4, 3, 2, 1],
+    M = from_list(List),
+    marray:sort(M),
+    ?assertEqual(marray:to_list(M), lists:sort(List)).
+
+reverse_test() ->
+    List = [1, 2, 3, 4, 5],
+    M = from_list(List),
+    marray:reverse(M),
+    ?assertEqual(marray:to_list(M), lists:reverse(List)).
+
+-endif.
